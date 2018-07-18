12bd58cf99de3688db2caae6521487ffc74be951
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2018-07-18 17:35:51 +0200

    Project updated

---
+++ src/com/sm/net/mo/view/MainViewEventHandler.java
@@ -0,0 +1,14 @@
+package com.sm.net.mo.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainViewEventHandler {
+
+	@FXML
+	private Button button;
+
+	public void buttonOnClick() {
+
+	}
+}
